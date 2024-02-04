0348bde94196505462c37875657e7a9bcf016e7d
Jobwafula <jobwafulabg@gmail.com>
2024-02-04 13:53:42 +0300

    2nd commmit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+const MY_API_KEY = '3f7f55253ef7776f44676c6d317f0be5'
 
 export default function Weather() {
     const [city, setCity] = useState('')
+    const [weatherData,setWeatherData] = useState(null) 
 
     // Handle input inchange
     const handleInputChange =(e)=>{
@@ -12,9 +14,16 @@ export default function Weather() {
     // fetch data
     const fetchData = async ()=>{
         // fetch from api
-    const response = await axios.get( `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid={YOUR_API_KEY}` )
+    const response = await axios.get( `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid={MY_API_KEY}` )
+    setWeatherData(response.data)
+    // see if data exists
+    console.log(weatherData)
 
     }
+
+    useEffect(()=>{
+        fetchData()
+    },[])
     // handlesubmit
     const handleSubmit = (e)=>{
         e.preventDefault()
